6351f778dc44edd14b2f5c53fa6cd4443497e896
Joe19071991 <lauranove66@gmail.com>
2025-02-14 10:51:09 +0100

    .

--- php/collection_list.php
+++ php/collection_list.php
@@ -26,15 +26,17 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ?>
 
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des Collectes</title>
-    <head>
+    
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&family=Lora:wght@400;700&family=Montserrat:wght@300;400;700&family=Open+Sans:wght@300;400;700&family=Poppins:wght@300;400;700&family=Playfair+Display:wght@400;700&family=Raleway:wght@300;400;700&family=Nunito:wght@300;400;700&family=Merriweather:wght@300;400;700&family=Oswald:wght@300;400;700&display=swap" rel="stylesheet">
-    </head>
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
 </head>
